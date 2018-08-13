88937c2a8e8b85ec17292f8207e6a8404d8e4f73
steinbrennergit <steinbrennergit@gmail.com>
2018-08-13 11:18:18 -0700

    incremental

--- models/pokemon.js
+++ models/pokemon.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
+  var Pokemon = sequelize.define("Pokemon", {
     text: DataTypes.STRING,
     description: DataTypes.TEXT
   });
-  return Example;
+  return Pokemon;
 };
